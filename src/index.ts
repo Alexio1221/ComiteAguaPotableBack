aa3e41651b4feef5a0b36910810669ae7d4466a8
Alexio1221 <alexgac31@gmail.com>
2025-07-31 21:18:04 -0400

    Se agrego el endpoint para registrar usuario

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv'; 
+import { registrarUsuario } from './controllers/auth/registrarPersona';
 
 dotenv.config(); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/auth', registrarUsuario);
 
 app.get('/', (_req, res) => {
   res.send('Servidor corriendo 🎉');
