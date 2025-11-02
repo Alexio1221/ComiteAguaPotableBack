5049a1778b6c155440afaff3e3887c293aa80c7f
Alexio1221 <alexgac31@gmail.com>
2025-11-02 18:07:54 -0400

    Se agrego la ruta para verficar si el usuario ya inicio sesion

--- src/routes/sesionRoutes.ts
+++ src/routes/sesionRoutes.ts
@@ -2,11 +2,17 @@ import { Router } from 'express';
 import { autenticar } from '../middlewar/autenticacion'
 import { iniciarSesion } from '../controllers/usuarios/loginController';
 import { cerrarSesion } from '../controllers/usuarios/loginController';
+import { RequestConUsuario } from '../middlewar/autenticacion';
 
 
 const router = Router();
 
 router.post('/login', iniciarSesion);  // Ruta para iniciar sesión
 router.post("/cerrar-sesion", autenticar, cerrarSesion);
+
+// Sirve para saber si un usuario inicio sesion o no, se usa en la vista principal
+router.get('/verificar-sesion', autenticar, (req: RequestConUsuario, res) => {
+  res.json({ loggedIn: true, user: req.user?.usuario });
+});
 
 export default router;
